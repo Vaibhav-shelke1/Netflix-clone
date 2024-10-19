7c9f3e2595e0bb9f24500e1f534217b383dc1129
Vaibhav-shelke1 <shelkevaibhav218@gmail.com>
2024-10-20 01:10:01 +0530

    login page

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,9 +1,35 @@
 import React from 'react';
 import './Login.css'
+import logo from "../../assets/logo.png"
 
 const Login = () => {
   return (
-    <div className='login'>Login</div>
+    <div className='login'>
+      <img src={logo} className='login-logo' alt="" />
+      <div className="login-form">
+        <h1>Sign Up</h1>
+        <form action="">
+          <input type="text" placeholder='Your name' />
+          <input type="email" placeholder='Email' />
+          <input type="password" placeholder='Password' />
+          <button>Sign Up</button>
+          <div className="form-help">
+            <div className="remember">
+              <input type="checkbox" />
+              <label htmlFor="">remember Me</label>
+            </div>
+            <p>Need Help?</p>
+          </div>
+
+
+        </form>
+        <div className="form-switch">
+          <p>New to Netflix <span>Sign Up Now</span></p>
+          <p>Already have account? <span>Sign in Now</span></p>
+
+        </div>
+      </div>
+    </div>
   )
 }
 
